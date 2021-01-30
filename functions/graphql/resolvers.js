edd30816f5e1053346da68f5a8334d874f380a1b
Shane Neubauer <shanen@gmail.com>
2021-01-30 22:46:11 +0100

    Created method to add a todo

--- functions/graphql/resolvers.js
+++ functions/graphql/resolvers.js
@@ -6,6 +6,15 @@ const resolvers = {
       return `Hello ${name || 'World'}`;
     },
   },
+
+  Mutation: {
+    async addTodo(_, { text }) {
+      const db = firebase.firestore();
+      const ref = db.collection('todos').doc();
+      await ref.set({ text });
+      return ref.id;
+    },
+  }
 };
 
 module.exports = resolvers;
--- functions/graphql/schema.js
+++ functions/graphql/schema.js
@@ -2,6 +2,10 @@ const typeDefs = `
   type Query {
     hello(name: String): String!
   }
+
+  type Mutation {
+    addTodo(text: String): String!
+  }
 `;
 
 module.exports = typeDefs;
